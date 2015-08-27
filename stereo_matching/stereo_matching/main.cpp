f5a51de754f65da505b71639b0044646ec5a230e
albert.liao <alb423@gmail.com>
2015-08-28 02:48:10 +0800

    modify to show image

--- stereo_matching/stereo_matching/main.cpp
+++ stereo_matching/stereo_matching/main.cpp
@@ -14,17 +14,19 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
-//#define PI 3.1415
+#define PI 3.1415
 using namespace cv;
 int main(int argc, char** argv)
 {
     //string pFile = (char*) argv[1];
-    string pFile = "EXAMPLE-f.ini";
-    string imgFile1 = "Fish-L.jpg";
-    string imgFile2 = "Fish-R.jpg";
-    string imgRecFile1 = ".Fish-L-rec-p-s.jpg";
+    string pFile = "/Users/miuki001/Work/opencv_test/stereo_matching/Images/EXAMPLE-f.ini";
+    //string pFile = "/Users/miuki001/Work/opencv_test/stereo_matching/Images/EXAMPLE-p.ini";
+    string imgFile1 = "/Users/miuki001/Work/opencv_test/stereo_matching/Images/Fish-L.jpg";
+    string imgFile2 = "/Users/miuki001/Work/opencv_test/stereo_matching/Images/Fish-R.jpg";
+    string imgRecFile1 = "Fish-L-rec-p-s.jpg";
     string imgRecFile2 = "Fish-R-rec-p-s.jpg";
     string imgDis = "Fish-dis.jpg";
+    
     
     //string pFile = "..\\..\\..\\Images\\EXAMPLE-p.ini";
     //string imgFile1 = "..\\..\\..\\Images\\Per-L.bmp";
@@ -34,11 +36,20 @@ int main(int argc, char** argv)
     //string imgDis = "..\\..\\..\\Images\\Per-dis.bmp";
     params_parser pp(pFile);
     
+    namedWindow( "Image1", WINDOW_AUTOSIZE );
+    namedWindow( "Image2", WINDOW_AUTOSIZE );
+    namedWindow( "RecImage1", WINDOW_AUTOSIZE );
+    namedWindow( "RecImage2", WINDOW_AUTOSIZE );
+    namedWindow( "DisImage", WINDOW_AUTOSIZE );
+    
     stereo st(CameraType::CT_FISHEYE, RectifyType::RT_PESPECTIVE);
     Mat R1,R2,P1,P2,Q;
     Mat img1 = imread(imgFile1);
     Mat img2 = imread(imgFile2);
     //Size imgsize = img1.size();
+    
+    imshow( "Image1", img1 );
+    imshow( "Image2", img2 );
     
     // use rectify function of stereo class
     st.stereoRectify(pp.cameraMatrix1, pp.cameraMatrix2, pp.R, pp.T, R1, R2, P1, P2);
@@ -68,11 +79,16 @@ int main(int argc, char** argv)
     st.initUndistortRectifyMap(pp.cameraMatrix2, pp.distCoeffs2, R2, cameraMatrixNew1, img2.size(), CV_32F, map21, map22, logRange, latRange);
     Mat recImg1, recImg2;
     st.rectifyImage(img1, img2, map11, map12, map21, map22, recImg1, recImg2);
+
+    imshow( "RecImage1", recImg1 );
+    imshow( "RecImage2", recImg2 );
     imwrite(imgRecFile1, recImg1);
     imwrite(imgRecFile2, recImg2);
     Mat dis;
     st.stereoMatching(recImg1, recImg2, dis, 0, 100/16*16, 5, 8*5*5, 32*5*5);
     dis.convertTo(dis, CV_8U);
+    imshow( "DisImage", dis );
     //imwrite(imgDis, dis);
+    waitKey(0);
     
 }
